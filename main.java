899273d420297b4df706e7ed46edeb6607a85e32
AKSHAY UPADHYAY <63426982+AkshayUpadhyay1899@users.noreply.github.com>
2026-03-10 17:13:22 +0530

    Added main.java file to calc branch

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Adding through calc: " + Calculator.add(2, 3));
     }
 }
